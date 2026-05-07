63e2547d471d190c15e3d8d73d579c8a2845eb69
Michael Davenport <mdaven19@byu.edu>
2026-05-06 20:17:39 -0600

    fix Authentication add timestamp

--- server/src/main/java/dev/mdaven/familygames/model/Authentication.java
+++ server/src/main/java/dev/mdaven/familygames/model/Authentication.java
@@ -2,4 +2,4 @@ package dev.mdaven.familygames.model;
 
 import java.util.UUID;
 
-public record Authentication (String token, UUID userId) {}
+public record Authentication (String token, UUID userId, long createdAt) {}
